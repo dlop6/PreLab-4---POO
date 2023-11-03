763116cb197df5895434f9a2d0c4b0bf54a28981
Diego L <lop23747@uvg.edu.gt>
2023-11-03 08:07:11 -0600

    Clases y directorios

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import Model.*;
+import org.json.simple.JSONArray;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {        
         System.out.println("Hello, World!");
     }
 }
