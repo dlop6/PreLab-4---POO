9ca9e04d0faac952e647e98f667e607a600f13ee
jlope384 <lop23415@uvg.edu.gt>
2023-11-03 08:02:30 -0600

    a

--- src/Model/PaymentInfo.java
+++ src/Model/PaymentInfo.java
@@ -9,52 +9,67 @@ public class PaymentInfo {
     private String cardNumber;
     private int expirationDate;
     private int cvv;
-    
+
     public String getUser() {
         return user;
     }
+
     public void setUser(String user) {
         this.user = user;
     }
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getPublicKey() {
         return publicKey;
     }
+
     public void setPublicKey(String publicKey) {
         this.publicKey = publicKey;
     }
+
     public double getAmount() {
         return amount;
     }
+
     public void setAmount(double amount) {
         this.amount = amount;
     }
+
     public int getInstallments() {
         return installments;
     }
+
     public void setInstallments(int installments) {
         this.installments = installments;
     }
+
     public String getCardNumber() {
         return cardNumber;
     }
+
     public void setCardNumber(String cardNumber) {
         this.cardNumber = cardNumber;
     }
+
     public int getExpirationDate() {
         return expirationDate;
     }
+
     public void setExpirationDate(int expirationDate) {
         this.expirationDate = expirationDate;
     }
+
     public int getCvv() {
         return cvv;
     }
+
     public void setCvv(int cvv) {
         this.cvv = cvv;
     }
